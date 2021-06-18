ced7eda81edf711a02a9acea984e410302908cbb
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-18 18:23:09 +0530

    Update social-scraper.js

--- plugins/social-scraper.js
+++ plugins/social-scraper.js
@@ -8,3 +8,18 @@
 
 */
 
+const Asena = require('../events');
+const { MessageType } = require('@adiwajshing/baileys');
+const got = require('got');
+//LyFE
+Asena.addCommand({ pattern: 'movie ?(.*)', fromMe: false, desc: "Shows movie info." }, (async (message, match) => {
+	if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
+	let url = `https://bx-hunter.herokuapp.com/api/fancytext?text=${match[1]}&apikey=ikygans`
+	const response = await got(url);
+	const json = JSON.parse(response.body);
+	if (json.Response != 'True') return await message.client.sendMessage(message.jid, '*Not found.*', MessageType.text, { quoted: message.data });
+	let msg = '💛';
+	msg += 'Styles      : ' + json.result + '\n\n';
+
+	await message.client.sendMessage(message.jid, msg, MessageType.text, { quoted: message.data });
+}));
