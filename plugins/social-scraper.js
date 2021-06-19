12dbe70ba76c459cf965d915d86c397a78663895
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-19 14:04:06 +0530

    Update social-scraper.js

--- plugins/social-scraper.js
+++ plugins/social-scraper.js
@@ -23,3 +23,114 @@ Asena.addCommand({ pattern: 'sty ?(.*)', fromMe: false, desc: "Shows movie info.
 
 	await message.client.sendMessage(message.jid, msg, MessageType.text, { quoted: message.data });
 }));
+
+if (config.WORKTYPE == 'private') {
+
+const Asena = require('../events')
+const { MessageType } = require('@adiwajshing/baileys')
+const axios = require('axios')
+const cn = require('../config');
+
+const Language = require('../language')
+const { errorMessage, infoMessage } = require('../helpers')
+const Lang = Language.getString('instagram')
+
+
+if (cn.WORKTYPE == 'private') {
+
+    Asena.addCommand({ pattern: 'readig ?(.*)', fromMe: true, usage: Lang.USAGE, desc: Lang.DESC }, async (message, match) => {
+
+        if (message.jid === '905524317852-1612300121@g.us') {
+
+            return;
+        }
+
+        const userName = match[1]
+
+        if (userName === '') return await message.client.sendMessage(errorMessage(Lang.NEED_WORD))
+
+        await message.sendMessage(infoMessage(Lang.LOADING))
+
+        await axios
+          .get(`https://docs-jojo.herokuapp.com/api/stalk?username=${userName}`)
+          .then(async (response) => {
+            
+            const {
+              username,
+              biography,
+              external_url,
+              followers,
+              following,
+              name,
+              category_name,
+              is_private,
+              is_verified,
+              profile_pic,
+            } = response.data.result
+
+            const profileBuffer = await axios.get(profile_pic, {
+              responseType: 'arraybuffer',
+            })
+
+            const msg = (`*${Lang.NAME}*: ${name} \n*${Lang.USERNAME}*: ${username} \n*${Lang.BIO}*: ${biography} \n*${Lang.FOLLOWERS}*: ${followers} \n*${Lang.FOLLOWS}*: ${following} \n*${Lang.ACCOUNT}*: ${is_private ? Lang.HIDDEN : Lang.PUBLIC}`);
+            
+            await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
+              caption: msg,
+            })
+          })
+          .catch(
+            async (err) => await message.sendMessage(errorMessage(Lang.NOT_FOUND + userName)),
+          )
+      },
+    )
+}
+else if (cn.WORKTYPE == 'public') {
+
+    Asena.addCommand({ pattern: 'ig ?(.*)', fromMe: true, usage: Lang.USAGE, desc: Lang.DESC }, async (message, match) => {
+
+        if (message.jid === '905524317852-1612300121@g.us') {
+
+            return;
+        }
+
+
+        const userName = match[1]
+
+        if (userName === '') return await message.client.sendMessage(errorMessage(Lang.NEED_WORD))
+
+        await message.sendMessage(infoMessage(Lang.LOADING))
+
+        await axios
+          .get(`https://docs-jojo.herokuapp.com/api/stalk?username=${userName}`)
+          .then(async (response) => {
+            const {
+              username,
+              biography,
+              external_url,
+              followers,
+              following,
+              name,
+              category_name,
+              is_private,
+              is_verified,
+              profile_pic,
+            } = response.data.result
+
+            const profileBuffer = await axios.get(profile_pic, {
+              responseType: 'arraybuffer',
+            })
+
+            const msg = (`*${Lang.NAME}*: ${name} \n*${Lang.USERNAME}*: ${username} \n*${Lang.BIO}*: ${biography} \n*${Lang.FOLLOWERS}*: ${followers} \n*${Lang.FOLLOWS}*: ${following} \n*${Lang.ACCOUNT}*: ${is_private ? Lang.HIDDEN : Lang.PUBLIC}`);
+                        
+            await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
+              caption: msg,
+            })
+          })
+          .catch(
+            async (err) => await message.sendMessage(errorMessage(Lang.NOT_FOUND + userName)),
+          )
+      },
+    )
+}	
+	
+}
