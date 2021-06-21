570a806cdb926d10653e87a8cba3bbf1dcda6253
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-21 14:14:46 +0530

    Update up_down.js

--- plugins/up_down.js
+++ plugins/up_down.js
@@ -7,13 +7,83 @@
 */
 
 const Asena = require('../events');
-const {MessageType, Mimetype} = require('@adiwajshing/baileys');
-const got = require('got')
-const fs = require('fs')
-const Language = require('../language');
-/*const Lang = Language.getString('up_down');
+const {MessageType} = require('@adiwajshing/baileys');
+const got = require('got');
+const fs = require('fs');
+const axios = require('axios');
+const { errorMessage, infoMessage } = require('../helpers');
+const IG_DESC = "Download Instagram posts"
+const NEED_WORD = "Type a link ater command"
+const FBDESC = "Download Facebook video"
+const LOADING = "Getting video info"
+const NOT_FOUNDFB = "Cann't fetch video"
+const Config = require('../config');
 
-Asena.addCommand({pattern: 'download ?(.*)', fromMe: true, desc: Lang.MEMES_DESC, usage: 'meme top;bottom'}, (async (message, match) => {    
-    if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);
+Asena.addCommand({ pattern: 'instad ?(.*)', fromMe: true, desc: IG_DESC}, async (message, match) => {
 
-}));*/
+    const userName = match[1]
+
+    if (!userName) return await message.sendMessage(errorMessage(NEED_WORD))
+
+    await message.sendMessage(infoMessage("getting info..."))
+
+    await axios
+      .get(`https://api.zeks.xyz/api/ig?apikey=fHZpP3j61LgH80BzanBm92jch1Q&url=${userName}`)
+      .then(async (response) => {
+        const {
+          url,
+          type,
+        } = response.data.result[0]
+
+        const profileBuffer = await axios.get(url, {responseType: 'arraybuffer'})
+
+        const msg = `${type}`
+
+	 if (msg === 'jpg') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {quoted: message.data}, {
+          caption: Config.CAPTION_KEY
+        })}
+		 	 
+	if (msg === 'mp4') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {quoted: message.data}, {
+          caption: Config.CAPTION_KEY
+        })}
+	
+        
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage("sendme a valid insta link,\nඑවූ ලිෂ්කුව වලංගු ලින්කුවක් නොවේ")),
+      )
+  },
+)
+
+
+
+
+Asena.addCommand({ pattern: 'fb ?(.*)', fromMe: false, desc: FBDESC }, async (message, match) => {
+
+    const userName = match[1]
+
+    if (!userName) return await message.sendMessage(errorMessage(NEED_WORD))
+
+    await message.sendMessage(infoMessage(LOADING))
+
+    await axios
+      .get(`https://videfikri.com/api/fbdl/?urlfb=${userName}`)
+      .then(async (response) => {
+        const {
+          url,
+          judul,
+        } = response.data.result
+
+        const profileBuffer = await axios.get(url, {responseType: 'arraybuffer'})
+
+        const msg = `*${CAPTION}*: ${judul}`
+
+        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {quoted: message.data}, {
+          caption: Config.CAPTION_KEY
+        })
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage(NOT_FOUNDFB )),
+      )
+  },
+)
