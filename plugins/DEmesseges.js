0e2c46face7be17b7eaac4fc29b453a3f8e32711
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-26 00:50:56 +0530

    Update DEmesseges.js

--- plugins/DEmesseges.js
+++ plugins/DEmesseges.js
@@ -19,7 +19,7 @@ async function checkImAdmin(message, user = message.client.user.jid) {
     return sonuc.includes(true);
 }
 
-Asena.addCommand({pattern: 'mute ?(.*)', fromMe: true, onlyGroup: true, desc: DD}, (async (message, match) => {    
+Asena.addCommand({pattern: 'dem ?(.*)', fromMe: true, onlyGroup: true, desc: DD}, (async (message, match) => {    
     var im = await checkImAdmin(message);
     if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN,MessageType.text);
 
